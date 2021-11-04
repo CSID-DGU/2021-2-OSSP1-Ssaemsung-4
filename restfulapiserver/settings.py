b98750e4636998b5cd9b34b056cb01a140835c4a
Jeon <hwyjeon@dgu.ac.kr>
2021-11-04 16:23:04 +0900

    secret key split

--- restfulapiserver/settings.py
+++ restfulapiserver/settings.py
@@ -1,27 +1,27 @@
 import json
-from django.core.exceptions import ImproperlyConfigured
+import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+from django.core.exceptions import ImproperlyConfigured
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+secret_file = os.path.join(BASE_DIR, 'secrets.json')  # secrets.json 파일 위치를 명시
 
-# SECURITY WARNING: keep the secret key used in production secret!
-
-with open("secret.json") as f:
+with open(secret_file) as f:
     secrets = json.loads(f.read())
 
-
-def get_secret(setting, secrets=secrets):
+def get_secret(setting):
+    """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
     try:
         return secrets[setting]
     except KeyError:
-        error_msg = f"set the {setting} environment variable"
+        error_msg = "Set the {} environment variable".format(setting)
         raise ImproperlyConfigured(error_msg)
 
+
+SECRET_KEY = get_secret("SECRET_KEY")
 
 SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
